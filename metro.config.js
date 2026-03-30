54be589f6f7e1631f3dd6d2009ce0519ae3c8b3f
Ujjwal Atri <ujjwalkrish082@gmail.com>
2026-03-30 23:45:06 +0530

    feat: add Google Sans font support and update metro config for extraNodeModules

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,14 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver = {
+	...config.resolver,
+	extraNodeModules: {
+		...(config.resolver?.extraNodeModules ?? {}),
+		'expo-keep-awake': path.resolve(__dirname, 'src/shims/expo-keep-awake-noop.ts'),
+	},
+};
 
 module.exports = config;
